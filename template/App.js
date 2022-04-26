471677b54953d0197382abf914bdfa245f60140d
slimecube98 <57504261+slimecube98@users.noreply.github.com>
2022-04-26 09:43:51 +0300

    Update App.js

--- template/App.js
+++ template/App.js
@@ -506,6 +506,7 @@ class App extends Component {
 
   backAction = e => {
     //this.webview.goBack();
+    this.triggerEvent('back_button');
     return true;
   };
 
@@ -550,8 +551,8 @@ class App extends Component {
     let read = PermissionsAndroid.check(
       PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
     );
-    let write = PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA);
-    let camera = PermissionsAndroid.check(
+    let camera = PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA);
+    let write = PermissionsAndroid.check(
       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
     );
     let location = PermissionsAndroid.check(
