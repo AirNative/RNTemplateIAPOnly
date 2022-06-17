ded224d8a1a6a6865611942ab46b87a22fffd423
slimecube98 <57504261+slimecube98@users.noreply.github.com>
2022-06-17 13:45:45 +0300

    Update App.js

--- template/App.js
+++ template/App.js
@@ -127,6 +127,13 @@ class App extends Component {
       });
       this.render();
     });
+    setTimeout(() => {
+      OneSignal.getDeviceState().then(data => {
+        if (data.isSubscribed == false) {
+          OneSignal.addTrigger("prompt_ios", "true");
+        }
+      });
+    },5000);
   }
 
   componentWillUnmount() {
